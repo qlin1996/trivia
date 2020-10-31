0b8b92e8a3c0d392203f37283bf0414488e650fd
qi lin <qlin1996@gmail.com>
2020-10-31 19:12:26 -0400

    updated answers to randomize

--- src/components/question.js
+++ src/components/question.js
@@ -1,21 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import NavBar from './navbar';
 import ScoreCard from './scoreCard';
 
 function Question({ setcurrentDisplay, shuffle, score, setScore, questions }) {
   const [currentQuestionNum, setcurrentQuestionNum] = useState(1);
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [answerOptions, setAnswerOptions] = useState([]);
 
   const question = questions[currentQuestionNum - 1].question;
-  const correctAnswer = questions[currentQuestionNum - 1].correct;
-  const inCorrectAnswer = questions[currentQuestionNum - 1].incorrect;
-  const answerOptions = [correctAnswer, ...inCorrectAnswer];
-  const shuffledAnswerOptions = shuffle(answerOptions, 4);
   console.log(questions[currentQuestionNum - 1]);
+
+  useEffect(() => {
+    const correctAnswer = questions[currentQuestionNum - 1].correct;
+    const inCorrectAnswer = questions[currentQuestionNum - 1].incorrect;
+    let answerOptions = [correctAnswer, ...inCorrectAnswer];
+    if (currentQuestionNum <= 10) {
+      answerOptions = shuffle(answerOptions, answerOptions.length);
+      setAnswerOptions(answerOptions);
+    }
+  }, [currentQuestionNum]);
 
   const onClick = (selectedAnswer) => {
     setIsSubmitted(true);
-    if (selectedAnswer === correctAnswer) setScore(score + 1);
+    if (selectedAnswer === questions[currentQuestionNum - 1].correct)
+      setScore(score + 1);
     setTimeout(() => {
       if (currentQuestionNum === 10) setcurrentDisplay('end');
       else setcurrentQuestionNum(currentQuestionNum + 1);
@@ -28,7 +36,7 @@ function Question({ setcurrentDisplay, shuffle, score, setScore, questions }) {
       <NavBar setcurrentDisplay={setcurrentDisplay} />
       <ScoreCard score={score} currentQuestionNum={currentQuestionNum} />
       <h2>{question}</h2>
-      {shuffledAnswerOptions.map((option, idx) => {
+      {answerOptions.map((option, idx) => {
         const isCorrect =
           option === questions[currentQuestionNum - 1].correct
             ? 'correct'
